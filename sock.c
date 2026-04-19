f2fab3bd3dbb21a5295c8fd810d0753755fef56b
Jakob Alvermark <jakob@alvermark.net>
2026-04-19 12:54:25 +0200

    Make WindDayMax repect -k (knots).

--- sock.c
+++ sock.c
@@ -352,6 +352,8 @@ int main(int argc, char **argv)
         short_int=(void *)(&buf[f+1]);
         f+=sizeof(short);
         t=(float)bswap16(*short_int)/10;
+        if(knots)
+          t=(float)bswap16(*short_int)/10*1.94384;
         printf("WindDayMax: %.01f\n",t);
         break;
       case 0x6c:
